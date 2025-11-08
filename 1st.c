f2da592e848323bc1f748fa530e6510ceee9dea1
Krish Bhingradiya <141948604+Krish-Bhingradiya-715@users.noreply.github.com>
2025-11-08 13:36:35 +0530

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+int getch(void);
+void ungetch(int);
+int getint(int *pn)
+{
+    int c, sign, next_c;
+    while (isspace(c = getch()))
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-') {
+        next_c = getch();
+        if (!isdigit(next_c)) {
+            ungetch(next_c);
+            ungetch(c);
+            return 0;
+        }
+        c = next_c;
+    }
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn *= sign;
+    if (c != EOF)
+        ungetch(c);
+    return c; 
+}
